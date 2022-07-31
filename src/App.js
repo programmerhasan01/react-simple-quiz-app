016d30d2b8fa0699cb73c46412b52528ccce5327
Hasan <programmerhasan.bd@gmail.com>
2022-07-31 21:57:41 +0600

    conditional rendering with stat quize btn and question card component and data fetching function and set on state

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
+import "./App.css";
+import QuestionCard from "./QuestionCard";
 function App() {
+  const [quizzes, setQuizzes] = useState(null);
+  const [startQuiz, setStartQuiz] = useState(false);
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [loaded, setLoaded] = useState(false);
+
+  const fetchQuiz = async () => {
+    const res = await fetch(
+      `https://opentdb.com/api.php?amount=5&category=18&difficulty=easy&type=multiple`
+    );
+    const { results } = await res.json();
+    setQuizzes(results);
+    setLoaded(true);
+    setStartQuiz(true);
+    console.log(results[0]);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="appContainer">
+        {!startQuiz && (
+          <div className="startQuiz">
+            <button onClick={fetchQuiz} className="startQuizBtn">
+              Start Quiz
+            </button>
+          </div>
+        )}
+        {loaded && <QuestionCard quiz={quizzes[currentQuestionIndex]} />}
+      </div>
     </div>
   );
 }
---
+++ src/QuestionCard.js
@@ -0,0 +1,6 @@
+const QuestionCard = ({ quiz }) => {
+  //   console.log(quiz);
+  return <div className="questionsContainer">{quiz.question}</div>;
+};
+
+export default QuestionCard;
